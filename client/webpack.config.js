1207c723d681f5bce2277f3c6c820f303303f5d4
Tracy Pham <phamtracy95@gmail.com>
2023-01-02 12:44:19 -0800

    Add webpack pwa manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,6 +27,24 @@ module.exports = () => {
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "JUST ANOTHER TEXT EDITOR",
+        short_name: "JATE",
+        description: "Take notes with Javascript text syntax highlighting!",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
