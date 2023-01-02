df59e4020214d427e21e5cd363aad19857d351a0
Tracy Pham <phamtracy95@gmail.com>
2023-01-02 14:56:23 -0800

    Update public url:

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -35,8 +35,8 @@ module.exports = () => {
         description: "Take notes with Javascript text syntax highlighting!",
         background_color: "#225ca3",
         theme_color: "#225ca3",
-        start_url: "/",
-        publicPath: "/",
+        start_url: "./",
+        publicPath: "./",
         icons: [
           {
             src: path.resolve("src/images/logo.png"),
